1c4379a2a6a7b7a8b430131eb8085a51af05a71a
Evgenij Karpov <gedrip87@gmail.com>
2021-09-13 11:33:38 +0300

    Homework2 is done

--- 1_date_and_time.py
+++ 1_date_and_time.py
@@ -1,3 +1,4 @@
+from datetime import date, datetime, timedelta
 """
 Домашнее задание №2
 
@@ -7,13 +8,19 @@
 2. Превратите строку "01/01/20 12:10:03.234567" в объект datetime
 
 """
+dt_now = datetime.now()
+delta = timedelta(days=1)
+delta2 = timedelta(days=30)
+yesterday = dt_now - delta
+days_ago_30 = dt_now - delta2
 
 def print_days():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    print(f'Вчера: {yesterday}',f'Сегодня: {dt_now}',f'30 дней назад: {days_ago_30}',sep='\n')
+    
 
 
 def str_2_datetime(date_string):
@@ -21,8 +28,8 @@ def str_2_datetime(date_string):
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    return datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
 
-if __name__ == "__main__":
+if __name__ == "__main__": 
     print_days()
-    print(str_2_datetime("01/01/20 12:10:03.234567"))
+    print(f'Перевод строки в дату: {str_2_datetime("01/01/20 12:10:03.234567")}')
--- 2_files.py
+++ 2_files.py
@@ -16,7 +16,15 @@ def main():
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
+    with open('referat.txt', 'r', encoding='utf-8') as f:
+        new_string = f.read()
+        print(f'Длинна строки: {len(new_string)} символа(ов)')
+        words = new_string.split()
+        print(f'Количество слов в тексте: {len(words)}')
+        modified_string = new_string.replace('.','!')
+
+    with open('referat2.txt', 'a', encoding='utf-8') as f:
+        f.write(modified_string)
 
 if __name__ == "__main__":
     main()
